898dcf9ac306fae67e6142c9df662bc66e1431d6
Dakshit Jha <jhadakshit2004@gmail.com>
2025-04-08 19:21:13 +0530

    few buds updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "Tangle",
+  description: "A Next.js 13 Meta Tangle application",
 };
 
 export default function RootLayout({
